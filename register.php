db9128d8a4786173db066208e4451a7cbf0a5b4e
catherinequassdorf <catherinequassdorf@hotmail.com>
2021-05-27 13:57:21 +0200

    jhk

--- register.php
+++ register.php
@@ -2,8 +2,8 @@
 
 <form method="POST" action="submit.php">
 						<label for="title">Username:</label>
-                        <input type="text" id="title" name="title" /><br />
+                        <input type="text" id="title" name="username" /><br />
                         <label for="isbn">Password:</label>
-                        <input type="text" id="isbn" name="isbn" />
+                        <input type="text" id="isbn" name="password" />
                         <input type="submit" value="Register!" name="submit" />
 					</form>
--- submit.php
+++ submit.php
@@ -1,23 +1,19 @@
 <!-- funkar ej -->
 <?php include('config.php');?>
 
-<title>Adding a book!</title>
+<title>Registering!</title>
 </head>
 
 <body>
-<h2>You submitted a book!</h2>
+<h2>Registration successful!</h2>
 
 <?php
-
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-
-$db = mysqli_connect('localhost', 'root', 'root', 'regn') or die('Error connecting');
-
-$query = "INSERT INTO customer (id, customerID, username, password, usertype) " . 
-"VALUES ('1', '$username', '$password', 0)";
+$query = "INSERT INTO customer(username, password, usertype) " . 
+"VALUES ('$username', '$password', 0)";
 
 $result = mysqli_query ($db, $query)
     or die('Error querying database.');

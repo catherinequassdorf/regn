8ce8dbc34b5dd340ec141ace7590555d9438f705
elsaandrsson <80895909+elsaandrsson@users.noreply.github.com>
2021-05-27 14:55:29 +0200

    hejhej

--- reserve.php
+++ reserve.php
@@ -1,3 +1,12 @@
 <?php include('templates/header.php');?>
 
 lägg till knappar som representerar städer där regn finns :P koppla dom till reservationssytem?
+
+
+<h4> vilken stad är du i </h4>
+
+<button> London </button>
+<button> Tokyo </button>
+<button> London </button>
+
+<h4> umbrellas available </h4>
